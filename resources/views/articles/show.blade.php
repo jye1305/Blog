db324b45e9ebe16695ddeb39bd2b27de2fb8af85
8Ren8 <lirenng95@1utar.my>
2022-04-13 21:54:51 +0800

    Show category in article

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -3,11 +3,7 @@
 <p>
     <strong>{{ $article['description']}}</strong><br>
     <strong>Author: {{ $article->user->name}}</strong>
-    @foreach($exhibition->art_objs as $art_obj) 
-        <tr>
-            <td>{{$exhibition->Ex_id}}</td>
-            <td>{{$art_obj->Id_no}}</td>
-            <td>{{$art_obj->Type_of_art}}</td>
-        </tr>
+    @foreach($article->categories as $category) 
+        <li>{{$category->category_name}}</li>
     @endforeach 
 </p>
