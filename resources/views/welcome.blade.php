28ce9368d8666f6ef8bd8ba292acccd70f27fc90
pyrox97 <chanliangj8273@gmail.com>
2022-04-15 00:15:59 +0800

    ntg to commit3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,8 +3,8 @@
 @section('content')
 
 
-    <img src="https://win10faq.com/wp-content/uploads/2019/04/welcome-blackboard.jpg" 
-        class="img-fluid" 
+    <img src="https://win10faq.com/wp-content/uploads/2019/04/welcome-blackboard.jpg"
+        class="img-fluid"
         alt="Responsive image">
 
 @endsection
